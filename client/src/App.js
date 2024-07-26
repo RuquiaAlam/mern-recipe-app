b1d9935a80867327ddf439d7894a96516090827c
Ruquia <ruquiaalam1826@gmail.com>
2024-07-25 17:53:43 -0700

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return <div className="App">
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
+  </div>;
 }
 
 export default App;
